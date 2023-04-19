b4a0b31b71817b0e1eef1291c48a5a5fc90f4fef
Boz Ntouran <duris2009@windowslive.com>
2023-04-19 19:39:54 +0300

    Initial commit

---
+++ src/main/java/bozduran/carreviewoauthserver/CarReviewOAuthServerApplication.java
@@ -0,0 +1,13 @@
+package bozduran.carreviewoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarReviewOAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarReviewOAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bozduran/carreviewoauthserver/CarReviewOAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bozduran.carreviewoauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarReviewOAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
